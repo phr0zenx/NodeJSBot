ef99e1bfcc8b3d8b9950a3aa5fa167a853f79145
phr0zenx <phr0zenx@gmail.com>
2017-12-01 23:39:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ function parseWikiJson(objJson) {
       var aryMatch = tmpJson.match(regPattern);
       if (aryMatch.length > 0) {
         output = '```' +
-          aryMatch[0].replace("\n","") + \n +
+          aryMatch[0].replace("\n","") + \
+          aryMatch[1].replace("\n","") + \
           '```'
       }
       return output;

f1fe8b7d1cccf6deca31119da6f389e5bffc01c4
phr0zenx <phr0zenx@gmail.com>
2017-12-01 20:25:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var http = require('http');
+var app = express();
+var options = {
+  host: 'aigis.wikia.com',
+  path: '/api.php?format=json&action=query&titles=Leeanne&prop=revisions&rvprop=content'
+};
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/:id', function(request, response) {
+  var strID = req.params.id;
+  if (!!strID) {
+    response.send('Hello World!');
+  }
+  
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});

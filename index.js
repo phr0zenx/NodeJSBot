7bdd128b74241887eef8c530230d0056fdd861c6
phr0zenx <phr0zenx@gmail.com>
2017-12-01 20:39:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-var options = {
-  host: 'aigis.wikia.com',
-  path: '/api.php?format=json&action=query&titles=Leeanne&prop=revisions&rvprop=content'
-};
+
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,7 +10,12 @@ app.use(express.static(__dirname + '/public'));
 app.get('/:id', function(request, response) {
   var strID = request.params.id;
   if (!!strID) {
-    response.send('Hello World!' + strID);
+    var httpquery = {
+      host: 'aigis.wikia.com',
+      path: '/api.php?format=json&action=query&titles=Leeanne&prop=revisions&rvprop=content'
+    };
+    http.request(httpquery, postHttp).end();
+    //response.send('Hello World!' + strID);
   } else {
     response.send('uhh');
   }
@@ -23,3 +25,18 @@ app.get('/:id', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
+
+postHttp = function(response) {
+   var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    //console.log(str);
+    response.send(str);
+  });
+}
